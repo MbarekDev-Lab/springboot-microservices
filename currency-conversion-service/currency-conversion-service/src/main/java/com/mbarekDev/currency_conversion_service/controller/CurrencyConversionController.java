e070d53fbf802073190ff88e940cf1358b7dd41a
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-07-29 23:47:50 +0200

    create service for currency conversion ms

---
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/mbarekDev/currency_conversion_service/controller/CurrencyConversionController.java
@@ -0,0 +1,51 @@
+package com.mbarekDev.currency_conversion_service.controller;
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import com.mbarekDev.currency_conversion_service.entities.CurrencyConversion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+@RestController
+public class CurrencyConversionController {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversion(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+		
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from",from);
+		uriVariables.put("to",to);
+		
+		ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+				CurrencyConversion.class, uriVariables);
+		
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+		
+		return new CurrencyConversion(currencyConversion.getId(), 
+				from, to, quantity, 
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment()+ " " + "rest template");
+		
+	}
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+	) {
+		return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+	}
+
+
+
+}
