bca9097d606a450f920f7fd1e8481bd567460128
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-07-30 07:52:00 +0200

    service registration

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) VALUES
+(10001, 'USD', 'MAD', 0.9, ''),
+(10002, 'EUR', 'MAD', 11, ''),
+(10003, 'MAD', 'EUR', 0.11, ''),
+(10004, 'MAD', 'USD', 9, '');
