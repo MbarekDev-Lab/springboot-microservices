6b86b1bc7452dd3c7e9059d0c5a825591f723faf
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-08-20 07:17:25 +0200

    Spring Boot build image command while skipping tests

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,6 +2,7 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// commandline to run mvn spring-build-image -DskipTests
 
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
